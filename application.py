71b73247a45da8ba838ad58eba8295a6867edff2
Chris Hill-Scott <me@quis.cc>
2020-03-09 13:25:47 +0000

    Add list page

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from data import get_closest, get_postage
+from data import get_closest, get_postage, lookup, make_key
 
 
 app = Flask(__name__)
@@ -27,3 +27,36 @@ def hello():
         result=result,
         postage=postage,
     )
+
+
+@app.route('/list')
+def country_list():
+    country_list = sorted([
+        str(canonical)
+        for canonical in set(lookup.values())
+    ])
+    synonyms = {
+        country: sorted([
+            key for key, value in lookup.items()
+            if value == country and key != make_key(country)
+        ])
+        for country in country_list
+    }
+    return render_template(
+        'list.html',
+        country_list=synonyms,
+    )
+
+
+@app.route('/list-raw')
+def list_raw():
+    country_list = sorted([
+        str(canonical)
+        for canonical in set(lookup.values())
+    ])
+    output = []
+    for country in country_list:
+        for key, value in sorted(lookup.items()):
+            if value == country and key != make_key(country):
+                output.append(f'({repr(key)}, {repr(country)}),')
+    return '\n'.join(sorted(output))
